5a9661a104a1c1d17587336c2fb37812fffbe61a
J. S. Brown <jsbrown7@gmail.com>
2026-04-20 10:10:49 -0700

    Palette changes across all files.

--- neovim.lua
+++ neovim.lua
@@ -8,23 +8,23 @@ return {
             colors = {
                 -- Monotone shades (base00-base07)
                 base00 = "#1a2238", -- Default background
-                base01 = "#48465a", -- Lighter background (status bars)
-                base02 = "#222c44", -- Selection background
-                base03 = "#5a5870", -- Comments, invisibles
-                base04 = "#909090", -- Dark foreground
-                base05 = "#cbcbcb", -- Default foreground
-                base06 = "#cbcbcb", -- Light foreground
-                base07 = "#909090", -- Light background
+                base01 = "#182133", -- Lighter background (status bars)
+                base02 = "#80889c", -- Selection background
+                base03 = "#9586cf", -- Comments, invisibles
+                base04 = "#afb6c5", -- Dark foreground
+                base05 = "#afb6c5", -- Default foreground
+                base06 = "#dde2eb", -- Light foreground
+                base07 = "#dde2eb", -- Light background
 
                 -- Accent colors (base08-base0F)
-                base08 = "#BD8767", -- Variables, errors, red
-                base09 = "#dbb9a5", -- Integers, constants, orange
-                base0A = "#b9a246", -- Classes, types, yellow
-                base0B = "#184c55", -- Strings, green
-                base0C = "#649FB6", -- Support, regex, cyan
-                base0D = "#6874bb", -- Functions, keywords, blue
-                base0E = "#434773", -- Keywords, storage, magenta
-                base0F = "#baaf80", -- Deprecated, brown/yellow
+                base08 = "#cf9276", -- Variables, errors, red
+                base09 = "#e2b099", -- Integers, constants, orange
+                base0A = "#d4be68", -- Classes, types, yellow
+                base0B = "#5f99a4", -- Strings, green
+                base0C = "#77b7c8", -- Support, regex, cyan
+                base0D = "#7f8fe0", -- Functions, keywords, blue
+                base0E = "#9586cf", -- Keywords, storage, magenta
+                base0F = "#e4d294", -- Deprecated, brown/yellow
             },
         },
         config = function(_, opts)
